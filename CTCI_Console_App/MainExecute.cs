ce751b11d07b2968b8102ab40f62402f51faa97c
popipepe <roberto.rivero95@gmail.com>
2017-08-31 14:44:13 -0500

    Init repo, cci training base + first problem

---
+++ CTCI_Console_App/MainExecute.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CTCI_Console_App
+{
+    class MainExecute
+    {
+        static void Main(string[] args)
+        {
+            bool continuar = true;
+            while (continuar)
+            {
+                RunProblems();
+                Console.WriteLine("Continue with more problems? y/n");
+                String info = Console.ReadLine();
+                if (info == "n")
+                {
+                    continuar = false;
+                }
+
+            }
+
+        }
+        public static void RunProblems()
+        {
+            Console.WriteLine("Please write the number of the problem you want");
+            int problem;
+            String[] problems = Enum.GetNames(typeof(probType));
+            for (int number = 1; number < problems.Length; number++)
+            {
+                Console.WriteLine((number) + "- " + problems[number]);
+            }
+
+            Problem DoThis;
+            bool good = int.TryParse(Console.ReadLine(), out problem);
+            if (!good)
+            {
+                DoThis = new Problem(probType.Z_Error_Not_Defined);
+            }
+            else
+            {
+                String name = Enum.GetName(typeof(probType), problem);
+                probType type = (probType)Enum.Parse(typeof(probType), name);
+                DoThis = new Problem(type);
+            }
+
+            DoThis.Solution();
+        }
+    }
+}
---
+++ CTCI_Console_App/ProblemMain.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CTCI_Console_App
+{
+    public partial class Problem
+    {
+        public probType type;
+
+        public Problem(probType type_I)
+        {
+            type = type_I;
+        }
+        public void Solution()
+        {
+            //Case calling the correct method from to solve the specific problem
+            if(type == probType.Z_Error_Not_Defined)
+            {
+                Console.WriteLine("Error Select a diferent problem");
+            }
+            else if (type == probType.IsUnique)
+            {
+                this.IsUnique();
+            }
+        }
+    }
+}
