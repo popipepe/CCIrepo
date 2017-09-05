33ff9185d2dbe5c71a001afad0615e9e090fa352
popipepe <roberto.rivero95@gmail.com>
2017-09-04 20:16:14 -0500

    Re-Order files

---
+++ CTCI_Console_App/Infrastructure/probType.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CTCI_Console_App
+{
+    public enum probType
+    {
+        //Used to use constants of probType Class.
+        Z_Error_Not_Defined = 0,
+
+        //Arrays and Strings.
+        IsUnique,
+        IsPermutation,
+        IsPalindromePermutation
+
+    }
+}
