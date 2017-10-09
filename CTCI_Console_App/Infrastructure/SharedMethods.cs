872abc26af4b43cbde3d38d6d11bd71f7c3e59e9
Roberto Rivero <roberto.rivero95@gmail.com>
2017-10-09 13:53:02 -0500

    getFileToArray added, need to check file path. Added test file challenge 335-1

--- CTCI_Console_App/Infrastructure/SharedMethods.cs
+++ CTCI_Console_App/Infrastructure/SharedMethods.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace CTCI_Console_App
 {
@@ -21,6 +22,25 @@ namespace CTCI_Console_App
             String word = Console.ReadLine();
 
             return word;
+        }
+        private String[] getFileToArrayInProjectHome()
+        {
+            Console.WriteLine("Write the name of the file to test");
+            String path = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) +
+                @"\Desktop\CCI_Project_And_Tests\CCI\trunk\CTCI_Console_App\ProblemTestFiles";
+            path = path + Console.ReadLine();
+            string[] lines;
+            List<String> listOfLines = new List<String>();
+            var fileStream = new FileStream(@path, FileMode.Open, FileAccess.Read);
+            using (var streamReader = new StreamReader(fileStream, Encoding.UTF8))
+            {
+                string line;
+                while ((line = streamReader.ReadLine()) != null)
+                {
+                    listOfLines.Add(line);
+                }
+            }
+            return lines = listOfLines.ToArray();
         }
     }
 }
